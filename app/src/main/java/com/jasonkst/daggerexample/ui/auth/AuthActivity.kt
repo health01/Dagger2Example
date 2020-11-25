3968ed62e9d8efba7e80bc2f16f5200fc1c10efd
jason <readonly23@gmail.com>
2020-11-25 23:38:00 +0000

    init authViewModel using the injected ViewModelProviderFactory

--- app/src/main/java/com/jasonkst/daggerexample/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/jasonkst/daggerexample/ui/auth/AuthActivity.kt
@@ -2,12 +2,19 @@ package com.jasonkst.daggerexample.ui.auth
 
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.RequestManager
 import com.jasonkst.daggerexample.R
-import dagger.android.DaggerActivity
+import com.jasonkst.daggerexample.viewModels.ViewModelProviderFactory
+import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
-class AuthActivity : DaggerActivity() {
+class AuthActivity : DaggerAppCompatActivity() {
+
+    lateinit var authViewModel: AuthViewModel
+
+    @Inject
+    lateinit var providerFactory: ViewModelProviderFactory
 
     @Inject
     lateinit var logo: Drawable
@@ -15,9 +22,11 @@ class AuthActivity : DaggerActivity() {
     @Inject
     lateinit var requestManager: RequestManager
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        setContentView(R.layout.activity_auth)
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+        authViewModel = ViewModelProvider(this,providerFactory).get(AuthViewModel::class.java)
         setLogo()
     }
 
