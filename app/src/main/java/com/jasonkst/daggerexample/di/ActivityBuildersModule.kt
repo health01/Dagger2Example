1ff03fcdd81d7e9e3f79928ab00baaee1c83e7f9
jason <readonly23@gmail.com>
2020-11-25 23:31:52 +0000

    1.Add AuthViewModelsModule into ActivityBuildersModule 2.Add ActivityBuildersModule into AppComponent

--- app/src/main/java/com/jasonkst/daggerexample/di/ActivityBuildersModule.kt
+++ app/src/main/java/com/jasonkst/daggerexample/di/ActivityBuildersModule.kt
@@ -1,11 +1,12 @@
 package com.jasonkst.daggerexample.di
 
+import com.jasonkst.daggerexample.di.auth.AuthViewModelsModule
 import com.jasonkst.daggerexample.ui.auth.AuthActivity
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBuildersModule {
-    @ContributesAndroidInjector
+    @ContributesAndroidInjector(modules = [AuthViewModelsModule::class])
     abstract fun contributeAuthActivity(): AuthActivity
 }
