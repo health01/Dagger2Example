f52653fa6c266a05544f2ae4d6d8ecb85e5e2bab
jason <readonly23@gmail.com>
2020-11-26 04:56:48 +0000

    Create AuthModule.kt to Provides a Api with context - AuthApi

---
+++ app/src/main/java/com/jasonkst/daggerexample/di/auth/AuthModule.kt
@@ -0,0 +1,12 @@
+package com.jasonkst.daggerexample.di.auth
+
+import com.jasonkst.daggerexample.network.auth.AuthApi
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+
+@Module
+object AuthModule {
+    @Provides
+    fun provideSessionApi(retrofit: Retrofit): AuthApi = retrofit.create(AuthApi::class.java)
+}
