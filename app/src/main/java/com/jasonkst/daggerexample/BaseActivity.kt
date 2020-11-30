8d86849b24e51e7e3fa89ddd82398ef6574411ad
jason <readonly23@gmail.com>
2020-11-30 00:58:47 +0000

    Added BaseActivity.kt and create MainActivity with logout function

---
+++ app/src/main/java/com/jasonkst/daggerexample/BaseActivity.kt
@@ -0,0 +1,57 @@
+package com.jasonkst.daggerexample
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import com.jasonkst.daggerexample.ui.auth.AuthActivity
+import com.jasonkst.daggerexample.ui.auth.AuthResource
+import dagger.android.support.DaggerAppCompatActivity
+import javax.inject.Inject
+
+abstract class BaseActivity : DaggerAppCompatActivity() {
+    private val TAG = "DaggerExample"
+
+    @Inject
+    lateinit var sessionManager: SessionManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        subscribeObservers()
+    }
+
+    private fun subscribeObservers() {
+        sessionManager.getAuthUser().observe(this, {
+            when (it) {
+                is AuthResource.Loading -> Log.d(
+                    TAG,
+                    "onChanged: BaseActivity: LOADING..."
+                )
+
+
+                is AuthResource.Authenticated ->
+                    Log.d(
+                        TAG,
+                        "onChanged: BaseActivity: AUTHENTICATED... " + "Authenticated as: " + it.data?.email.toString()
+                    )
+
+
+                is AuthResource.Error -> Log.d(TAG, "onChanged: BaseActivity: ERROR...")
+
+                is AuthResource.Logout -> {
+                    Log.d(
+                        TAG,
+                        "onChanged: BaseActivity: NOT AUTHENTICATED. Navigating to Login screen."
+                    )
+                    navLoginScreen()
+                }
+            }
+        })
+    }
+
+    private fun navLoginScreen() {
+        Intent(this, AuthActivity::class.java).let {
+            startActivity(it)
+            finish()
+        }
+    }
+}
