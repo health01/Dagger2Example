3ebb3945cfbf315394d81e343b8bbe6406f7157f
jason <readonly23@gmail.com>
2020-12-01 23:30:43 +0000

    Create MainModule and added to the ActivityBuildersModule

--- app/src/main/java/com/jasonkst/daggerexample/di/ActivityBuildersModule.kt
+++ app/src/main/java/com/jasonkst/daggerexample/di/ActivityBuildersModule.kt
@@ -3,6 +3,7 @@ package com.jasonkst.daggerexample.di
 import com.jasonkst.daggerexample.di.auth.AuthModule
 import com.jasonkst.daggerexample.di.auth.AuthViewModelsModule
 import com.jasonkst.daggerexample.di.main.MainFragmentBuildersModule
+import com.jasonkst.daggerexample.di.main.MainModule
 import com.jasonkst.daggerexample.di.main.MainViewModelsModule
 import com.jasonkst.daggerexample.ui.auth.AuthActivity
 import com.jasonkst.daggerexample.ui.main.MainActivity
@@ -15,7 +16,7 @@ abstract class ActivityBuildersModule {
     @ContributesAndroidInjector(modules = [AuthViewModelsModule::class, AuthModule::class])
     abstract fun contributeAuthActivity(): AuthActivity
 
-    @ContributesAndroidInjector(modules = [MainFragmentBuildersModule::class, MainViewModelsModule::class])
+    @ContributesAndroidInjector(modules = [MainFragmentBuildersModule::class, MainViewModelsModule::class,MainModule::class])
     abstract fun contributeMainActivity(): MainActivity
     /*  @Binds
       @IntoMap
