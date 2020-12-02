26e709c457745e9393f02e410d0e3cce973c58fd
jason <readonly23@gmail.com>
2020-12-02 01:47:30 +0000

    Added Post request api

--- app/src/main/java/com/jasonkst/daggerexample/network/main/MainApi.kt
+++ app/src/main/java/com/jasonkst/daggerexample/network/main/MainApi.kt
@@ -3,9 +3,12 @@ package com.jasonkst.daggerexample.network.main
 import com.jasonkst.daggerexample.models.Post
 import io.reactivex.Flowable
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface MainApi {
-    @GET("users/{id}")
-    fun getUser(@Path("userId") id: Int): Flowable<List<Post>>
+    // /posts?userId=1/
+    @GET("posts")
+    fun getPostsFromUser(
+        @Query("userId") id: Int
+    ): Flowable<List<Post>>
 }
