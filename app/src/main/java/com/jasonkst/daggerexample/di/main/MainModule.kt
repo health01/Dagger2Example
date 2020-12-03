ceab963b5a7387611c833e6d2f5e3d90124a2087
jason <readonly23@gmail.com>
2020-12-03 21:10:36 +0000

    Added di for adapter and LinearLayoutManager

--- app/src/main/java/com/jasonkst/daggerexample/di/main/MainModule.kt
+++ app/src/main/java/com/jasonkst/daggerexample/di/main/MainModule.kt
@@ -1,7 +1,9 @@
 package com.jasonkst.daggerexample.di.main
 
-import com.jasonkst.daggerexample.network.auth.AuthApi
+import android.app.Application
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jasonkst.daggerexample.network.main.MainApi
+import com.jasonkst.daggerexample.ui.main.posts.PostRecyclerAdapter
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
@@ -10,4 +12,10 @@ import retrofit2.Retrofit
 object MainModule {
     @Provides
     fun provideSessionApi(retrofit: Retrofit): MainApi = retrofit.create(MainApi::class.java)
+
+    @Provides
+    fun provideAdapter(): PostRecyclerAdapter = PostRecyclerAdapter()
+
+    @Provides
+    fun provideLinearLayoutManager(application: Application,): LinearLayoutManager = LinearLayoutManager(application)
 }
