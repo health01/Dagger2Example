2df00937fe6971abf2139602b846fdc2df33215e
jason <readonly23@gmail.com>
2020-12-03 22:34:35 +0000

    Added DrawerLayout for MainActivity.kt

--- app/src/main/java/com/jasonkst/daggerexample/ui/main/MainActivity.kt
+++ app/src/main/java/com/jasonkst/daggerexample/ui/main/MainActivity.kt
@@ -3,26 +3,26 @@ package com.jasonkst.daggerexample.ui.main
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.jasonkst.daggerexample.BaseActivity
 import com.jasonkst.daggerexample.R
 import com.jasonkst.daggerexample.ui.main.posts.PostsFragment
-import com.jasonkst.daggerexample.ui.main.profile.ProfileFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
+
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
     private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, TAG, Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setSupportActionBar(toolbar)
+        // 將drawerLayout和toolbar整合，會出現「三」按鈕
         testFragment()
+        setToolBar()
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
     private fun testFragment() {
@@ -30,6 +30,19 @@ class MainActivity : BaseActivity() {
             .replace(R.id.main_container, PostsFragment())
             .commit()
     }
+
+    private fun setToolBar() {
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            toolbar,
+            R.string.nav_app_bar_open_drawer_description,
+            R.string.nav_app_bar_navigate_up_description
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -48,5 +61,21 @@ class MainActivity : BaseActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_profile -> {
+                println(TAG)
+            }
+
+            R.id.nav_posts -> {
+                println(TAG)
+            }
+        }
+        //close navigation drawer
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
