8d40a068e594a62fe75cb20dfefeccba04c3c6f3
jason <readonly23@gmail.com>
2020-12-03 21:16:49 +0000

    Show the data

--- app/src/main/java/com/jasonkst/daggerexample/ui/main/posts/PostsFragment.kt
+++ app/src/main/java/com/jasonkst/daggerexample/ui/main/posts/PostsFragment.kt
@@ -6,20 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jasonkst.daggerexample.R
+import com.jasonkst.daggerexample.ui.main.Resource
 import com.jasonkst.daggerexample.viewModels.ViewModelProviderFactory
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_posts.*
 import javax.inject.Inject
 
 class PostsFragment : DaggerFragment() {
     private val TAG = "PostsFragment"
 
     lateinit var viewModel: PostsViewModel
-    private val recyclerView: RecyclerView? = null
 
     @Inject
     lateinit var providerFactory: ViewModelProviderFactory
+
+    @Inject
+    lateinit var postAdapter: PostRecyclerAdapter
+
+    @Inject
+    lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,7 +38,10 @@ class PostsFragment : DaggerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         viewModel = ViewModelProvider(this, providerFactory).get(PostsViewModel::class.java)
+
+        initRecyclerView()
         subscribeObservers()
     }
 
@@ -39,8 +49,33 @@ class PostsFragment : DaggerFragment() {
         viewModel.observePosts().apply {
             removeObservers(viewLifecycleOwner)
             observe(viewLifecycleOwner, {
+                when (it) {
+                    is Resource.Loading -> Log.d(
+                        TAG,
+                        "onChanged: PostsFragment: LOADING..."
+                    )
+
+                    is Resource.Success -> {
+                        Log.d(TAG, "onChanged: PostsFragment: got posts.")
+                        it.data?.let { it1 -> postAdapter.setPosts(it1) }
+                    }
+
+                    is Resource.Error -> {
+                        Log.d(
+                            TAG,
+                            "onChanged: PostsFragment: ERROR... " + it.message
+                        )
+                    }
+                }
                 Log.d(TAG, "onChanged: " + it.data)
             })
+        }
+    }
+
+    private fun initRecyclerView() {
+        recycler_view.apply {
+            layoutManager = linearLayoutManager
+            adapter = postAdapter
         }
     }
 }
